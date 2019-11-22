5f18fbc54b520c1fed7bab37acf4c9d870f6481a
Prateekgautam21 <49075034+Prateekgautam21@users.noreply.github.com>
2019-11-22 16:43:19 +0530

    Update urls.py

--- geekapp/urls.py
+++ geekapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from geekapp import views
 
 urlpatterns = [
-    path('index/',views.index ,name='index'),
+    path('',views.index ,name='index'),
     path('host/',views.host, name='host'),
     path('end/',views.end, name="end"),
     path('welcome/<name>/<pk>',views.submittedform, name='submittedform')
